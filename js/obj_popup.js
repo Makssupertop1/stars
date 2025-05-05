90e9fe6c0a250fec3e58d257802d8e8a27e3a29e
Makssupertop1 <masteroforion55@gmail.com>
2025-05-05 20:23:18 +0300

    Add files via upload

---
+++ js/obj_popup.js
@@ -0,0 +1,153 @@
+// Солнце
+const SunPopup = new Popup({
+    id: "sun-popup",
+    backgroundColor: "#040013ff",
+    borderWidth: "10px",
+    borderColor: "#f39f18ff",
+    titleColor: "#f39f18ff",
+    textColor: "#f39f18ff",
+    //---------------------
+    title: "Главная последовательность",
+    content: `Главная последовательность-это стадия эволюции звёзд, а также область на диаграмме Герцшпрунга — Рассела, образованная звёздами на этой стадии, и соответствующий класс светимости.`,
+    showImmediately: false
+});
+
+function open_sun(){
+    SunPopup.show()
+}
+
+// Меркурий
+const MercuryPopup = new Popup({
+    id: "mercury-popup",
+    backgroundColor: "#040013ff",
+    borderWidth: "10px",
+    borderColor: "#f8f8fbff",
+    titleColor: "#f8f8fbff",
+    textColor: "#f8f8fbff",
+    //---------------------
+    title: "Коричневый карлик",
+    content: `Коричневые карлики – Это космические тела, занимающие промежуточное положение между планетами и звёздами. От обычных звёзд коричневые карлики отличаются тем, что температура в их недрах никогда не достигает значений, необходимых для протекания термоядерной реакции превращения лёгкого изотопа водорода  в гелий, которая обеспечивает длительное свечение обычных звёзды
+        `,
+    showImmediately: false
+});
+
+function open_mercury(){
+    MercuryPopup.show()
+}
+
+// Венера
+const VenusPopup = new Popup({
+    id: "venus-popup",
+    backgroundColor: "#040013ff",
+    borderWidth: "10px",
+    borderColor: "#ffcf48ff",
+    titleColor: "#ffcf48ff",
+    textColor: "#ffcf48ff",
+    //---------------------
+    title: "Красный карлик",
+    content: `Красные карлики — это небольшие и сравнительно холодные звезды, которые не отличаются высокой яркостью по сравнению с другими светилами. Их жизненный цикл довольно продолжителен.
+        `,
+    showImmediately: false
+});
+
+function open_venus(){
+    VenusPopup.show()
+}
+
+// Земля
+const EarthPopup = new Popup({
+    id: "earth-popup",
+    backgroundColor: "#040013ff",
+    borderWidth: "10px",
+    borderColor: "#98fb98ff",
+    titleColor: "#98fb98ff",
+    textColor: "#98fb98ff",
+    //---------------------
+    title: "Жёлтый карлик",
+    content: `Желтые карлики (солнце подобные звёзды)звёзды на пути которых есть этап красного гиганта, где объём звёзды увеличивается в разы. После стадии красного гиганта звезда сбрасывает свою оболочку и получатся туманность и маленький белый карлик, а потом эта туманность становить похожей на взрыв сверхновой.
+        `,
+    showImmediately: false
+});
+
+function open_earth(){
+    EarthPopup.show()
+}
+
+// Марс
+const MarsPopup = new Popup({
+    id: "mars-popup",
+    backgroundColor: "#040013ff",
+    borderWidth: "10px",
+    borderColor: "#e52b50ff",
+    titleColor: "#e52b50ff",
+    textColor: "#e52b50ff",
+    //---------------------
+    title: "Марс",
+    content: `
+        `,
+    showImmediately: false
+});
+
+function open_mars(){
+    MarsPopup.show()
+}
+
+// Луна
+const MoonPopup = new Popup({
+    id: "moon-popup",
+    backgroundColor: "#040013ff",
+    borderWidth: "10px",
+    borderColor: "#c5d0e6ff",
+    titleColor: "#c5d0e6ff",
+    textColor: "#c5d0e6ff",
+    //---------------------
+    title: "Голубые гиганты",
+    content: `Голубые гиганты — это массивные, чрезвычайно яркие звезды. Их отличает характерный голубой цвет, который указывает на очень высокую температуру поверхности. Эти звезды зачастую во много раз массивнее нашего Солнца и могут быть в десятки раз более яркими.
+
+Голубые гиганты находятся на продвинутой стадии звездной эволюции. Они быстро сжигают свое ядерное топливо и имеют относительно короткую по астрономическим масштабам продолжительность жизни — от нескольких миллионов до нескольких десятков миллионов лет. Обычно они образуются в очень плотных  и объёмных  звёздных областей.
+
+        `,
+    showImmediately: false
+});
+
+function open_moon(){
+    MoonPopup.show()
+}
+
+// Фобос
+const PhobosPopup = new Popup({
+    id: "phobos-popup",
+    backgroundColor: "#040013ff",
+    borderWidth: "10px",
+    borderColor: "#ab274fff",
+    titleColor: "#ab274fff",
+    textColor: "#ab274fff",
+    //---------------------
+    title: "Фобос",
+    content: `
+        `,
+    showImmediately: false
+});
+
+function open_phobos(){
+    PhobosPopup.show()
+}
+
+// Деймос
+const DeimosPopup = new Popup({
+    id: "deimos-popup",
+    backgroundColor: "#040013ff",
+    borderWidth: "10px",
+    borderColor: "#ab274fff",
+    titleColor: "#ab274fff",
+    textColor: "#ab274fff",
+    //---------------------
+    title: "Деймос",
+    content: `
+        `,
+    showImmediately: false
+});
+
+function open_deimoss(){
+    DeimosPopup.show()
+}
